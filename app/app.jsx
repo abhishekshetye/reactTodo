4aea1941ca5f8a0fd9608a6acb53ee43030b4f97
abhishek <abhi15kk@gmail.com>
2017-01-20 22:10:58 +0530

    Added redux store

--- app/app.jsx
+++ app/app.jsx
@@ -5,7 +5,17 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
 
+var actions = require('actions');
+var store = require('configureStore').configure();
 
+
+store.subscribe(() => {
+	console.log('new state', store.getState())
+})
+
+store.dispatch(actions.addTodo('clean the yard'))
+store.dispatch(actions.setSearchText('yard'))
+store.dispatch(actions.toggleShowCompleted())
 
 $(document).ready(function($) {
     $(document).foundation();
